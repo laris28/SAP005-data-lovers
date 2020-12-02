12a411445d1e838c8250496c0a21275bb1f403cf
laris28 <larissalmeida2002@gmail.com>
2020-12-02 18:31:29 -0300

    Arquivo novo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,48 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {select, ordenarPokemons} from './data.js';
+import data from "./data/pokemon/pokemon.js";
 
-console.log(example, data);
+const todosPokemons = data.pokemon;
+const div = document.querySelector("#card");
+
+function geraCards(array){
+  let card = ""
+  for (let pokemon of array) {
+    card += `
+      <article class="divcard">
+        <p class="numero">Nº ${pokemon.num}</p>
+        <p class="nome">${pokemon.name.toUpperCase()}</p>
+        <img class="imagem" src="${pokemon.img}">
+        <p class="texto-card">Tipo: ${pokemon.type.join(", ")}</p>
+        <p class="texto-card">Resistências: ${pokemon.resistant.join(", ")}</p>
+        <p class="texto-card">Fraquezas: ${pokemon.weaknesses.join(", ")}</p>
+      </article>`
+  }
+  return card
+}
+
+div.innerHTML = geraCards(data.pokemon)
+console.log(data.pokemon);
+// console.log(data);
+
+//FILTRO
+const selecionar = document.getElementById("filterType");
+selecionar.addEventListener("change", pegarTipo);
+
+function pegarTipo() {
+  const filtroType = document.getElementsByClassName("filterType").value;
+  console.log(filtroType)
+  const filtrado = select(todosPokemons, "normal");
+  geraCards(filtrado);
+}
+
+//ORDENAÇÃO
+const selectOrdenar = document.getElementById("ordenar");
+if(selectOrdenar){
+selectOrdenar.addEventListener("change", pegarOrdem);
+}
+
+function pegarOrdem() {
+    const filtroOrdenar = document.getElementById("ordenar").value;
+    const ordered = ordenarPokemons(todosPokemons, filtroOrdenar);
+    geraCards(ordered);
+}
