200fcdb8aaba41f45eb0c607f2c82ead7ab0b8bb
laris28 <larissalmeida2002@gmail.com>
2020-12-03 21:46:14 -0300

    teste e funções

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,50 @@
-import { example, anotherExample } from '../src/data.js';
+import {select} from '../src/data.js';
 
+const mock =  [{
+  "num": "001",
+  "name": "bulbasaur",
+  "type": [
+    "grass",
+    "poison"
+  ]
+  },
+{
+  "num": "002",
+  "name": "ivysaur",
+  "type": [
+    "normal",
+    "poison"
+  ]
+}]
+
+const pokemonNormal = [
+  {
+    "num": "002",
+    "name": "ivysaur",
+    "type": [
+      "normal",
+      "poison"
+    ]
+  }
+]
 
 describe('example', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof select).toBe('function');
   });
 
   it('returns `example`', () => {
-    expect(example()).toBe('example');
+    expect(select(mock, "normal")).toStrictEqual(pokemonNormal);
   });
 });
 
 
 describe('anotherExample', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    // expect(typeof anotherExample).toBe('function');
   });
 
   it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+    // expect(anotherExample()).toBe('OMG');
   });
 });
